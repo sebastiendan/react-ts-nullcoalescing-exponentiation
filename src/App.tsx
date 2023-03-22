cc2bbdd6b46a77d30964b811dd054603f6ebf7c1
sebastiendan <sebastien.dan@gmail.com>
2023-03-22 11:55:04 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+import { RLP } from '@ethereumjs/rlp'
+import { Trie } from '@ethereumjs/trie'
+import { Alert, Layout } from 'antd'
+import React from 'react'
+
+const App = () => {
+  const trie = new Trie()
+
+  return (
+    <></>
+  )
+}
+
+export default App
